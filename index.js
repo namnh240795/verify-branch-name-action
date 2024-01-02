1c601f681c0110c90c052e0ce3ee2c7135a5683c
Nam Nguyen <namnh240795@gmail.com>
2024-01-02 14:14:00 +0700

    feat: update usage

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { Octokit } = require("@octokit/core");
 
 
 try {
-    const githubToken = core.getInput('github-token');
     const regex = core.getInput('regex');
-    const octokit = new Octokit({ auth: githubToken });
-    
     const branchName = github.context.payload.pull_request.head.ref;
-    console.log(`Branch name: ${branchName}`, regex);
-    // // fetch pr commit
-    // octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/commits', {
-    //     owner: github.context.repo.owner,
-    //     repo: github.context.repo.repo,
-    //     pull_number: prNumber
-    // }).then((response) => {
-
-    //         if (!regex.test(commit)) {
-    //             core.setFailed(`Invalid branch name.\n`);
-    //             return;
-    //         }
-    // });
-    core.setOutput("Pass", " See convention.md for more details.\n");
+    if (!regex) {
+        core.setFailed(`Invalid regex.\n`, "See convention.md for more details.\n");
+        return;
+    }
+    if (!branchName) {
+        core.setFailed(`Invalid branch name.\n`, "See convention.md for more details.\n");
+        return;
+    }
+    if (!regex.test(branchName)) {
+        core.setFailed(`Invalid branch name.\n`, "See convention.md for more details.\n");
+        return;
+    }
+  
+    core.setOutput("Pass", true);
     
 } catch (error) {
     core.setFailed(error.message);
